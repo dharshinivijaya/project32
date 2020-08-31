86b8b77a9e90c253e12eefb3755673534c5b19c2
dharshinivijaya <67852865+dharshinivijaya@users.noreply.github.com>
2020-08-31 17:14:07 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,24 @@
+class Box extends BaseClass {
+  constructor(x, y, width, height){
+    super(x,y,width,height);
+    this.image = loadImage("sprites/wood1.png");
+  }
+
+};
+
+Score();
+blocks1.score();
+blocks2.score();
+blocks3.score();
+blocks4.score();
+blocks5.score();
+blocks6.score();
+blocks7.score();
+blocks8.score();
+blocks9.score();
+
+score(){
+  if(this.visibility<0 && this.visibility >=105){
+    score++;
+    }
+}
